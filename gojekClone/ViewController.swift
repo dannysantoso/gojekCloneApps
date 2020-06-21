e37c521e50e9bac347faf9e8655499e7ff75bfe8
dannysantoso <danny.sntoso@gmail.com>
2020-06-21 18:08:17 +0700

    Initial Commit

---
+++ gojekClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gojekClone
+//
+//  Created by danny santoso on 21/06/20.
+//  Copyright © 2020 danny santoso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
